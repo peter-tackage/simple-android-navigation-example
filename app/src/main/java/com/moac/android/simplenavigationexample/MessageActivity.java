df4917698e620549a92cf1f306d2b3e5d8b71268
Peter Tackage <peter.tackage@futurice.com>
2015-09-17 15:58:33 +0300

    Add empty MessageActivity

---
+++ app/src/main/java/com/moac/android/simplenavigationexample/MessageActivity.java
@@ -0,0 +1,14 @@
+package com.moac.android.simplenavigationexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+
+}
